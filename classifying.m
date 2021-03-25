38a95095b5de139cfe7f163f2f0f7f9c2fe95258
joao-hg-marques <81370082+joao-hg-marques@users.noreply.github.com>
2021-03-25 14:28:59 +0000

    Classifying Images
    This script allows classifying images previoulsly download from the web. The classification will be given according to the trained data

--- classifying.m
+++ classifying.m
@@ -4,11 +4,11 @@ file=uigetfile;
 I=imread(file);
 figure
 imshow(I);
+%The image will resize at the first input layer
 sz = netTransfer.Layers(1).InputSize;
 I = imresize(I,sz(1:2));
-%figure
-%imshow(I);
 label = classify(netTransfer,I);
+%It classifies according to the trained dataset
 figure("Name","Image Classification","NumberTitle","off");%%%%
 imshow(I)
 title(label);
